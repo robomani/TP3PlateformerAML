57c49f591afc9d93928b43e58f5ffd20cd7ee284
robomani <robomani@hotmail.com>
2018-07-13 12:12:01 -0400

    commit avant fin de semaine

---
+++ TP3Plateformer/Assets/Scripts/Barrel.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrel : MonoBehaviour
+{
+    [SerializeField]
+    private float m_TurnSpeed = 10f;
+    [SerializeField]
+    private float m_LunchForce = 250f;
+
+    private bool m_PlayerLocked;
+    private PointEffector2D m_Effector;
+    private Rigidbody2D m_LockedBody;
+
+    private void Awake()
+    {
+        m_Effector = GetComponent<PointEffector2D>();
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.forward * (m_TurnSpeed * Time.deltaTime)); 
+
+        if (m_PlayerLocked && m_LockedBody != null)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                
+                m_Effector.enabled = false;
+                m_LockedBody.constraints = RigidbodyConstraints2D.None;
+                m_LockedBody.constraints = RigidbodyConstraints2D.FreezeRotation;
+                m_LockedBody.AddForce(transform.up * m_LunchForce);
+                StartCoroutine(RestartEffector());
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D i_Collision)
+    {
+        if (i_Collision.tag == "Player" && !m_PlayerLocked)
+        {
+            m_LockedBody = i_Collision.GetComponent<Rigidbody2D>();
+            float PosX = i_Collision.transform.position.x;
+            float PosY = i_Collision.transform.position.y;
+            if ((PosX >= transform.position.x - 1.5 || PosX <= transform.position.x + 1.5) && (PosY >= transform.position.y - 1.5 || PosY <= transform.position.y + 1.5))
+            {
+                m_LockedBody.velocity = Vector3.zero;
+                m_LockedBody.constraints = RigidbodyConstraints2D.FreezeAll;
+                i_Collision.transform.position = transform.position;
+                m_PlayerLocked = true;
+            }
+        }
+    }
+
+    private IEnumerator RestartEffector()
+    {
+        yield return new WaitForSeconds(2);
+        m_PlayerLocked = false;
+        m_Effector.enabled = true;
+        m_LockedBody = null;
+    }
+}
---
+++ TP3Plateformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody2D m_RB;
+    public float m_Speed = 10f;
+    public float m_JumpForce = 50f;
+
+    
+    private Vector2 m_MoveDir = new Vector2();
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            //m_MoveDir = transform.right;
+            m_RB.AddForce(transform.right * m_Speed);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            //m_MoveDir = -transform.right;
+            m_RB.AddForce(-transform.right * m_Speed);
+        }
+        else
+        {
+            m_MoveDir = Vector2.zero;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && m_RB.velocity.y == 0)
+        {
+            m_RB.AddForce(transform.up * m_JumpForce);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        //m_MoveDir *= m_Speed;
+        m_MoveDir.y = m_RB.velocity.y;
+        //m_RB.velocity = m_MoveDir;
+    }
+}
